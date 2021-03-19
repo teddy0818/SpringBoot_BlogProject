e163c7c525f31eeb33f4765679dc8151d049c7f0
Byeongjin Hwang <taddy0818@naver.com>
2021-03-19 21:49:30 +0900

    lombok 세팅 및 테스트

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -17,10 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("http/lombok")
+	public String lombokTest() {
+//		Member m = new Member(1, "ssar", "1234", "email");
+		// Builder 어노테이션을 사용하면 이렇식으로 쓸수있음 (id 가 db 에서 시퀀스로 자동으로 1씩 증가시키고싶다~ 할때 쓰임)
+		// 또 실수할일이 줄어든다!!
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter : " + m.getId());
+		 m.setId(5000);
+		System.out.println(TAG+"setter : " + m.getId());
+		return "lombok test 완료";
+	}
+	
 	// 인터넷 브라우저 요청은 무조건 get 방식만 가능함!!
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest(Member m) { // id=1&username=ssar&password=1234&email=ssar@naver.com //  자동으로 spring이 값을 넣어줌 ( MessageConverter (스프링부트))
+	public String getTest(Member m) { // id=1&username=ssar&password=1234&email=ssar@naver.com //  자동으로 spring이 값을 넣어줌 ( MessageConverter (스프링부트))	
+		
 		return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail() ;
 	}
 	
--- blog/src/main/java/com/cos/blog/test/Member.java
+++ blog/src/main/java/com/cos/blog/test/Member.java
@@ -1,47 +1,44 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+// @Getter
+// @Setter
+//@RequiredArgsConstructor // final 붙은애들 생성자 만듬 
+@Data // getter setter 둘다 만듬 
+// @AllArgsConstructor // 생성자 만듬 
+@NoArgsConstructor // 파라미터가 없는 생성자 생성 
 public class Member {
 	
 	// 변수를 priavate 로 만드는 이유 ? 바로 접근해서 값을 변경한다? 이건 마법이다! 객체지향에 어울리지 않는다! 메소드로 접근하자!
+	// final 은 불변성을 위해 (데이터를 바꿀 생각이 있으면 final 붙이면 안됨)
+	
+//	private final int id;
+//	private final String username;
+//	private final String password;
+//	private final String email;
 	
 	private int id;
-	private String username;
+	private  String username;
 	private String password;
 	private String email;
 	
-	//생성자 
+	// @AllArgsConstructor로 대체 가
+	
+	@Builder
 	public Member(int id, String username, String password, String email) {
-		this.id = id; 
+		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	
 	
 	
 }
