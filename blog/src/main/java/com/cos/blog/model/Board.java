96974a9321f46cc6218369a5cba46cda70a3350a
Byeongjin Hwang <taddy0818@naver.com>
2021-04-03 16:55:30 +0900

    board model 수정 - reply와 join 방법 이해

--- blog/src/main/java/com/cos/blog/model/Board.java
+++ blog/src/main/java/com/cos/blog/model/Board.java
@@ -1,16 +1,19 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -40,11 +43,17 @@ public class Board {
 	@ColumnDefault("0") // int 형이니까 홑따옴표 (') 필요없음 
 	private int count; // 조회수 
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER) //  FetchType.EAGER  값을 바로 가져온다 
 	// Many = Board, One = User // 한명의 유저가 여러개의 글을 쓸 수 있다 
 	@JoinColumn(name="userId")
 	private User user; // DB는 오브젝트를 저장할수 없다. 그레서 Foreign key를 쓰는데, 자바는 오브젝트를 저장할 수 있다.
 	// User 객체니 User 테이블을 자동으로 참조한다 (Foreign key)
+	
+	// 하나의 게시물에 여러 댓글을 쓸 수 있다 // 외래키가 필요없다.
+	// mappedBy 연관관계의 주인이 아니다. 난 외래키가 아니에요! DB의 칼럼을 만들지 마세요!
+	//  FetchType.LAZY : 값을 나중에 들고 올수 있다. ex) 댓글 보기 클릭시 reply객체 들고오기. but 이 프로젝트에선 한번에 다 가져오니까 FetchType.EAGER 쓰자
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
